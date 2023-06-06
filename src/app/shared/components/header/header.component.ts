2ab6d331b03ba6232b7c89893dbb58ef70c11943
razvanbalasa40 <razvanbalasa40@gmail.com>
2023-06-06 14:59:38 +0300

    Modified header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -13,17 +13,19 @@ export class HeaderComponent {
   public burgerSrc = 'assets/img/burger-menu-icon.svg';
 
   toggleMenu() {
-    this.menuToggled = !this.menuToggled;
-    if (this.menuToggled) {
-      this.burgerSrc = 'assets/img/close-menu-icon.svg';
-      document.body.style.height = '100vh';
-      document.body.style.width = '100vw';
-      document.body.style.overflow = 'hidden';
-    } else {
-      this.burgerSrc = 'assets/img/burger-menu-icon.svg';
-      document.body.style.height = 'initial';
-      document.body.style.width = 'initial';
-      document.body.style.overflow = 'initial';
+    if (window.innerWidth <= 768) {
+      this.menuToggled = !this.menuToggled;
+      if (this.menuToggled) {
+        this.burgerSrc = 'assets/img/close-menu-icon.svg';
+        document.body.style.height = '100vh';
+        document.body.style.width = '100vw';
+        document.body.style.overflow = 'hidden';
+      } else {
+        this.burgerSrc = 'assets/img/burger-menu-icon.svg';
+        document.body.style.height = 'initial';
+        document.body.style.width = 'initial';
+        document.body.style.overflow = 'initial';
+      }
     }
   }
 }
