b2e41834cc51d3ddf7c14405c1541089c1382668
razvanbalasa40 <razvanbalasa40@gmail.com>
2023-06-08 19:50:16 +0300

    Added footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Gtag } from 'angular-gtag';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+  constructor(private gtag: Gtag) {}
 
+  redirectToSocial(socialPlatform: string) {
+    this.gtag.event('footer_redirect_to_social', {
+      platform: socialPlatform,
+    });
+  }
+
+  goToAppStore(ctaElement: string) {
+    this.gtag.event('go_to_app_store', {
+      cta: ctaElement,
+    });
+  }
 }
